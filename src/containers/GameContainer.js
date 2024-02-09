ef9485fce7c31a81a41a800ecf0a1d18ab90b549
DSoutter <duncan.soutter@gmail.com>
2024-02-09 22:12:46 +0000

    Added buttons and styles from web

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -1,12 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import HeaderComponent from '../components/HeaderComponent';
 import FooterComponent from '../components/FooterComponent';
-
+import '../styles/Game.css'
 const GameContainer = () => {
 
     return (
         <div id="game-container">
             <HeaderComponent/>
+            <button className="button" onClick={(event) => {console.log("Start")}}><span>Start Game?</span></button>
+            <br/>
+            <button className="button"><span>High Scores</span></button>
             <div>
                 Game Container
             </div>
