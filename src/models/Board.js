a0fe3496e5a077afa9e3506a5ee6f98d3b74a905
DSoutter <duncan.soutter@gmail.com>
2024-02-20 20:37:46 +0000

    Board model skeleton

---
+++ src/models/Board.js
@@ -0,0 +1,30 @@
+import Cell from './Cell.js';
+
+class Board {
+    constructor(rows, columns){
+    this.rows = 20;
+    this.columns = 10;
+    this.grid = this.createEmptyGrid();
+    }
+
+    createEmtpyGrid() {
+        return Array.from({length: this.rows}, () => Array(this.columns).fill(null));
+    }
+
+    ifValidMove(piece, offsetX, offsetY) {
+
+    }
+
+    placePiece(piece, offsetX, offsetY) {
+
+    }
+
+    clearLines() {
+
+    }
+
+    isGameOver() {
+
+    }
+}
+export default Board;
