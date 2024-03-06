1e8a874ea35eafb74f2eaa32bf3a32287a0b1eaf
DSoutter <duncan.soutter@gmail.com>
2024-03-06 21:54:59 +0000

    Created High Scores draft

---
+++ src/components/HighScores.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HighScoresCompponent = () => {
+    return(
+        <div>
+            <h1>High Scores:</h1>
+            <li>
+                <ol>First</ol>
+                <ol>Second</ol>
+                <ol>Third</ol>
+            </li>
+        </div>
+    )
+}
+export default HighScoresCompponent 
