260038ecf615359b8c13e99fd7bb14c4b4b5023a
DSoutter <duncan.soutter@gmail.com>
2024-03-13 20:55:44 +0000

    Got routing working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React, {useEffect, useState, event} from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './styles/Game.css';
+import GameContainer from './containers/GameContainer';
+import AboutComponent from './components/AboutComponent';
+import HighScoresComponent from './components/HighScores';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
-import GameContainer from './containers/GameContainer';
+
 function App() {
   return (
+    <Router>
       <div className="Game">
         <HeaderComponent/>
-        <GameContainer/>
+        <Routes>
+          <Route path="/" exact component={GameContainer}/>
+          <Route path="/about" element={<AboutComponent />} /> {AboutComponent}
+          <Route path="/highscores" element={<HighScoresComponent />} />{HighScoresComponent}/>
+        </Routes>
         <FooterComponent/>
       </div>
+    </Router>
   );
 }
 
