0e17be9203c887c22f13e2d2da2144584e2a61bf
DSoutter <duncan.soutter@gmail.com>
2024-03-22 13:00:16 +0000

    Created components for output

---
+++ src/components/Cell.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Cell = ({type}) => {
+    return (
+        <div>Cell</div>
+    );
+};
+export default Cell;
---
+++ src/components/Stage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Cell from "./Cell";
+
+const Stage = ({stage}) => {
+    return(
+        <div><Cell type="empty"/></div>
+    )
+}
+
+export default Stage;
