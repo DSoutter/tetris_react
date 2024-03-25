6aeaa71434f3949bb8e68ffad22746ec1efb66d5
DSoutter <duncan.soutter@gmail.com>
2024-03-25 20:39:07 +0000

    Simplified navigation code

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,31 +1,23 @@
 import React from 'react';
-import {Link, useNavigate} from 'react-router-dom';
-import '../styles/HeaderAndFooter.css'
-import { StyledButton } from './styles/StyledButton';
+import {useNavigate} from 'react-router-dom';
 import StartButton from './StartButton';
-import { StyledBars } from './styles/StyledBars';
+import {StyledButton} from './styles/StyledButton';
+import {StyledBars} from './styles/StyledBars';
 
 const HeaderComponent = () => {
     const navigate = useNavigate();
 
-    const navigateToAbout = () => {
-        navigate('/about');
+    const navigateToPath = (path) => {
+        navigate(path);
       };
-    const navigateToHighScores = () => {
-        navigate('/highscores');
-      };
-    
-    const navigateToGame = () => {
-      navigate('/');
-    }
 
     return(
       <StyledBars>
         <h2>Tetris React App</h2>
         <div>
-          <StyledButton onClick={navigateToGame}>Game</StyledButton>
-          <StyledButton onClick={navigateToAbout}>About</StyledButton>
-          <StyledButton onClick={navigateToHighScores}>High Scores</StyledButton>
+          <StyledButton onClick={() =>navigateToPath('/')}>Game</StyledButton>
+          <StyledButton onClick={() =>navigateToPath('/about')}>About</StyledButton>
+          <StyledButton onClick={() =>navigateToPath('/highscores')}>High Scores</StyledButton>
         </div>
       </StyledBars>  
     )   
