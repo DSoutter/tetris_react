90c5339871d3384b8b7aa7b2c018201fb3bfa9f8
DSoutter <duncan.soutter@gmail.com>
2024-03-25 15:28:59 +0000

    Changed display method

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,16 +1,9 @@
 import React from "react";
 import Cell from "./Cell";
-import "./../styles/Game.css"
-const Stage = ({grid}) => {
+const Stage = ({stage}) => {
     return (
         <div className="board">
-          {grid.map((row, rowIndex) => (
-            <div key={rowIndex} className="row">
-              {row.map((cell, colIndex) => (
-                <Cell key={`${rowIndex}-${colIndex}`} value={cell} />
-              ))}
-            </div>
-          ))}
+            {stage.map(row => row.map((cell, x) => (<Cell key={x} type={cell[0]} />)))}
         </div>
       );
     };
