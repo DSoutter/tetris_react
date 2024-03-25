52951fc04693102a6f60eea718a17b161ddb371f
DSoutter <duncan.soutter@gmail.com>
2024-03-25 20:26:50 +0000

    Added some styles

--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -4,27 +4,16 @@ export const StyledTetrisWrapper = styled.div`
     width: 100vw;
     height: 100vh;
     background-color: gray;
+    overflow: hidden;
     `
 
 export const StyledTetris  = styled.div`
-
-.board {
-  display: grid;
-  grid-template-columns: repeat(10, 20px);
-  grid-template-rows: repeat(20);
-  border: 2px solid rgb(0, 0, 0);
-  margin: 0 auto;
-  width: fit-content;
-  height: 400px;
-}
-
-.cell {
-  background-color: black;
-  border: 1px solid grey;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+display: flex;
+justify-content: space-evenly;
+align-items: center;
+padding: 40px;
+margin: 0 auto;
+max-width: 900px;
 aside {
     width: 100%;
     max-width: 200px;
